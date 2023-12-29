0d7dd118a2e9f9f206dd62d3763806e217ca21e3
Senne Vansant <91118511+VansantSenne@users.noreply.github.com>
2023-12-29 14:00:59 +0100

    Update BeoordelingsRepository.java

--- beoordelingsservice/src/main/java/fact/it/beoordelingservice/repository/BeoordelingsRepository.java
+++ beoordelingsservice/src/main/java/fact/it/beoordelingservice/repository/BeoordelingsRepository.java
@@ -5,5 +5,4 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.List;
 public interface BeoordelingsRepository extends MongoRepository<Beoordeling, String> {
-    List<Beoordeling> findByCodeIn(List<String> code);
 }
