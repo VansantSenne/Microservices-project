dd9295570bf9a6dbe640a075ffba883e54988a06
Senne Vansant <91118511+VansantSenne@users.noreply.github.com>
2023-12-29 14:10:49 +0100

    Update BeoordelingsController.java

--- beoordelingsservice/src/main/java/fact/it/beoordelingservice/controller/BeoordelingsController.java
+++ beoordelingsservice/src/main/java/fact/it/beoordelingservice/controller/BeoordelingsController.java
@@ -1,4 +1,27 @@
 package fact.it.beoordelingservice.controller;
 
+import fact.it.beoordelingservice.dto.BeoordelingsResponse;
+import fact.it.beoordelingservice.model.Beoordeling;
+import fact.it.beoordelingservice.service.BeoordelingsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/beoordeling")
+@RequiredArgsConstructor
 public class BeoordelingsController {
+
+    private final BeoordelingsService beoordelingsService;
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<BeoordelingResponse> getAllBeoordelingen() {
+        return beoordelingsService.getAllBeoordelingen();
+    }
 }
+
