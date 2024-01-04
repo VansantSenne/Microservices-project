0899733bc842c476b1ceda3d747c32e22fa3168c
samuel <r0879834@student.thomasmore.be>
2024-01-04 13:59:45 +0100

    long

--- beoordelingsservice/src/main/java/fact/it/beoordelingservice/repository/BeoordelingsRepository.java
+++ beoordelingsservice/src/main/java/fact/it/beoordelingservice/repository/BeoordelingsRepository.java
@@ -4,5 +4,5 @@ import fact.it.beoordelingservice.model.Beoordeling;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.List;
-public interface BeoordelingsRepository extends MongoRepository<Beoordeling, String> {
+public interface BeoordelingsRepository extends MongoRepository<Beoordeling, Long> {
 }
